0462e405d001b8e0b03cbf40c3cf00bd0b416c8f
iamismail <ismailmbarouk@gmail.com>
2025-02-26 12:40:51 +0300

    Color Toggler  Project Added

---
+++ 19.Color Toggler/app.js
@@ -0,0 +1,11 @@
+let toggler = document.getElementById("switch");
+
+toggler.addEventListener("click", () => {
+    // if(toggler.checked === true){
+    //     document.body.style.background = "black";
+    // }else{
+    //     document.body.style.background = "white";
+    // }
+
+    toggler.checked === true ? (document.body.style.background = "black") : (document.body.style.background = "white")
+})
