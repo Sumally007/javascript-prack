667ae87efaffe33e798bacc5c65913c26cd13498
iamismail <ismailmbarouk@gmail.com>
2025-02-27 12:17:14 +0300

    Random Emojies Project Added

---
+++ 21.Random Emojies/app.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("#emoji");
+
+const emojis = ["😉", "😁", "😊", "😂", "🤣", "😍", "😒", "😘", "😢", "😎", "😜", "😃", "🤢", "🤔", "😆", "😴", "🥱", "😫", "😪", "😤", "🙃", "😕", "😞", "🙁", "🤑", "🤐" ]
+
+
+btn.addEventListener("mouseover", () => {
+    btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)]
+})
